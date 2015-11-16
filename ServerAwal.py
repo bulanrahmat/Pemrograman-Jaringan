ccd1205fca3e9ce9bff3e447b4552ce705ce3406
bulanrahmat <bulanrahmatsetyadi@gmail.com>
2015-11-16 10:32:58 +0700

    Program dengan memakai kodingan dasar Pak Royan yang masih eror

--- ServerAwal.py
+++ ServerAwal.py
@@ -2,6 +2,7 @@ import threading
 import socket
 import time
 import sys
+import re
 
 def get_file(nama):
 	myfile = open(nama)
@@ -18,22 +19,35 @@ class MemprosesClient(threading.Thread):
 	def run(self):
 		message = ''
 		while True:
-        		data = self.client_socket.recv(32)
-            		if data:
-				message = message + data #collect seluruh data yang diterima
-				if (message.endswith("\r\n\r\n")):
-					self.client_socket.send(get_file('gambar.jpg'))
-					break
+			self.client_socket, self.my_socket = self.my_socket.accept()
+    			print "Connection from: " + `caddr`
+    			data = self.client_socket.recv(1024)
+    			print data
+    			match = re.match('GET /gambar\=(\d+)\sHTTP/1', data)
+            		if match:
+				angle = match.group(1)
+        			print angle
+        			if angle == "1":
+            				f=open("history.jpg","r+")
+           				gambar=f.read()
+            				f.close()
+            				self.client_socket("HTTP/1.1 200 OK \r\n\r\n%s"%gambar)
+				elif angle == "2":
+	    				f=open("gambar.jpg","r+")
+            				gambar=f.read()
+           				f.close()
+            				self.client_socket.sendall("HTTP/1.1 200 OK \r\n\r\n%s"%gambar)
             		else:
-               			break
-		self.client_socket.close()
+				print "Returning 404"
+       				self.client_socket.sendall("HTTP/1.0 404 Not Found\r\n")
+    				self.client_socket.close()
 		
 
 
 class Server(threading.Thread):
 	def __init__(self):
 		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.server_address = ('localhost',9999)
+		self.server_address = ('127.0.0.1',10002)
 		self.my_socket.bind(self.server_address)
 		threading.Thread.__init__(self)
 
